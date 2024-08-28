c09a756927fbee0c13bd0b14e7178a71fdfc4346
Nitin1solutions <nitin@1solutions.biz>
2024-08-28 11:04:29 +0530

    loading css

--- app/loading.js
+++ app/loading.js
@@ -5,7 +5,7 @@ export default function Loading() {
   return (
     <div className="main-loader">
       <div style={{ width: 'auto', height: '70px' }}>
-        <Image src={LoadImage} width={200} height={100} alt="Loading" style={{width:'200px!important' ,height:'100px!important'}}/>
+        <Image src={LoadImage} width={200} height={100} alt="Loading" />
       </div>
     </div>
   );
