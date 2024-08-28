b188b344adbe0f5aa1ad0ae0753edb4b3e508e60
Nitin1solutions <nitin@1solutions.biz>
2024-08-28 10:52:38 +0530

    change css file

--- app/loading.js
+++ app/loading.js
@@ -5,7 +5,7 @@ export default function Loading() {
   return (
     <div className="main-loader">
       <div style={{ width: 'auto', height: '70px' }}>
-        <Image src={LoadImage} width={200} height={100} alt="Loading" />
+        <Image src={LoadImage} width={200} height={100} alt="Loading" style={{width:'200px!important' ,height:'100px!important'}}/>
       </div>
     </div>
   );
